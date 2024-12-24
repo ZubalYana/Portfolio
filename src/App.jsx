316adc901365e3461c6c62327f4589991151cf7b
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-12-24 06:39:50 +0200

    set up react routes and create 2 main routes of the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-import './App.css'
-export default function App() {
-  return (
-    <div>My portfolio here!</div>
-  )
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AboutMe from './components/AboutMe/AboutMe';
+import Projects from './components/Projects/Projects';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<AboutMe />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+        </Router>
+    );
 }
+
+export default App;
