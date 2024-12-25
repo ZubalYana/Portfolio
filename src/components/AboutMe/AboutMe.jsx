25893859b29280350d440b476892539404a64426
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-12-25 08:37:51 +0200

    set and style background text

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 export default function AboutMe() {
   return (
     <div className="wrap w-full min-h-screen bg-customBlack">
+      <section className='w-full h-[90vh] p-5 flex flex-col items-center relative'>
+        <p className='text-customPurple text-9xl absolute top-[70%] w-[900px] flex justify-between'><span className='backgroundText'>Yana</span><span className='backgroundText'>Zubal</span></p>
+      </section>
     </div>
   )
 }
