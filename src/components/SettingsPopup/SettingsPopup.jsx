58e9b3de75df286456648ea1f6ba3eee32bf9a2c
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-12-26 06:59:18 +0200

    style the settings popup and import the svgs for settings popup animation

--- src/components/SettingsPopup/SettingsPopup.jsx
+++ src/components/SettingsPopup/SettingsPopup.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Modal from 'react-modal';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import './SettingsPopup.css';
 Modal.setAppElement('#root'); 
 
 const SettingsPopup = ({ isOpen, onClose }) => {
@@ -17,7 +19,7 @@ const SettingsPopup = ({ isOpen, onClose }) => {
           alignItems: 'center',
         },
         content: { 
-          padding: '20px', 
+          padding: '25px', 
           borderRadius: '16px', 
           width: '650px', 
           height: '290px',
@@ -25,12 +27,26 @@ const SettingsPopup = ({ isOpen, onClose }) => {
           top: '50%',
           left: '50%',
           transform: 'translate(-50%, -50%)',
+          color: '#1F1F1F',
         },
       }}
     >
-      <h2>Settings</h2>
-      <p>Shared settings for both pages.</p>
-      <button onClick={onClose}>Close</button>
+      <FontAwesomeIcon icon={faXmark} className="closeButton absolute top-[25px] right-[25px] cursor-pointer" onClick={onClose} size="2x" />
+      <h2 className='popupTitle text-4xl font-normal mb-4'>{'<Settings/>'}</h2>
+      <div className="settingGroup flex mb-3">
+        <p className='font-light text-xl mr-1'>Page language:</p>
+        <select id="language" className='font-medium text-xl'>
+          <option value="en" selected>Eng.</option>
+          <option value="ua">Укр.</option>
+          <option value="fr">Fr.</option>
+          <option value="es">Esp.</option>
+          <option value="it">It.</option>
+        </select>
+      </div>
+      <div className="settingGroup flex">
+        <p className='font-light text-xl mr-1'>Page theme:</p>
+        <p className='font-medium text-xl cursor-pointer'>dark ( default )</p>
+      </div>
     </Modal>
   );
 };
