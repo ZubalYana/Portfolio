bd8340187290dcd3cba0086db8549e5443fd3cd1
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-12-30 18:26:47 +0200

    create a static version of the footer

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -15,6 +15,7 @@ import EducationCards from '../EducationCards';
 import ProjectsCards from '../ProjectsCards';
 import ExploreBtn from '../ExploreBtn/ExploreBtn';
 import Questions from '../Questions/Questions';
+import footerLogo from '/footer logo.png';
 export default function AboutMe() {
       
   return (
@@ -71,10 +72,54 @@ export default function AboutMe() {
          <ProjectsCards />
          <ExploreBtn />
       </section>
-      <section className='w-full h-[100vh] px-10 py-5 flex flex-col items-center'>
+      <section className='w-full h-[100vh] px-10 py-10 flex flex-col items-center'>
          <h3 className="sectionTitle">{'<FAQ/>'}</h3>
          <h3 className="sectionSubtitle text-base font-light text-customWhite w-[700px] text-center">Have questions? Search here! There are my answers to the most frequent questions. If you didn’t find what you are looking for, contact me! </h3>
          <Questions />
+      </section>
+      <section className='w-full h-[53vh] px-10 py-5 flex flex-col'>
+        <h3 className="sectionTitle">{'<Get in touch!/>'}</h3>
+        <div className='w-full h-[1px] rounded-sm bg-customPurple mt-2'></div>
+        <div className="footerContent w-full flex justify-between mt-7">
+          <div className="footerLeftPart">
+          <img src={footerLogo} alt="futer logo" className='w-[180px]' />
+          <p className='w-[420px] font-light text-base text-customWhite mt-2'>Got interested? Have questions or suggestions? Contact me for cooperation! I’d like to discuss any information with you and work together!</p>
+          <div className="socialMediaIcons w-[280px] flex justify-between items-center mt-6">
+          <a href="https://github.com/ZubalYana" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="2x" className="text-customPurple hover:text-customWhite transition-colors duration-300" />
+          </a>
+          <a href="https://www.instagram.com/yanavesq/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} size="2x" className="text-customPurple hover:text-customWhite transition-colors duration-300" />
+          </a>
+          <a href="https://t.me/yanavesq" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faTelegram} size="2x" className="text-customPurple hover:text-customWhite transition-colors duration-300" />
+          </a>
+          <a href="mailto:zubalana0@gmail.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faEnvelope} size="2x" className="text-customPurple hover:text-customWhite transition-colors duration-300" />
+          </a>
+          <a href="tel:+1234567890" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faPhone} size="2x" className="text-customPurple hover:text-customWhite transition-colors duration-300" />
+          </a>
+
+        </div>
+          </div>
+          <div className="footerRightPart w-[360px] flex justify-between">
+            <div className='flex flex-col items-end'>
+              <h4 className='text-2xl font-semibold text-customPurple'>Navigation</h4>
+              <p className='font-light text-sm text-customWhite mt-2 cursor-pointer'>Who am I?</p>
+              <p className='font-light text-sm text-customWhite mt-2 cursor-pointer'>Skills</p>
+              <p className='font-light text-sm text-customWhite mt-2 cursor-pointer'>Projects</p>
+              <p className='font-light text-sm text-customWhite mt-2 cursor-pointer'>Education & Experience</p>
+              <p className='font-light text-sm text-customWhite mt-2 cursor-pointer'>FAQ</p>
+            </div>
+            <div className='flex flex-col items-end'>
+              <h4 className='text-2xl font-semibold text-customPurple'>Contacts</h4>
+              <p className='font-light text-sm text-customWhite mt-2 cursor-pointer'>+380 97 205 87 86</p>
+              <p className='font-light text-sm text-customWhite mt-2 cursor-pointer'>zubalana0@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <div className="copyright w-full flex justify-center text-customWhite mt-7 font-light text-xs">Copyright © 2024 <span className='copyrightAuthor'>Yana Zubal</span>. All rights reserved.</div>
       </section>
     </div>
   );
