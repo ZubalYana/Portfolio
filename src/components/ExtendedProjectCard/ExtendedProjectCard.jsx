6ac3e4bba74ebb11b1999f7c44a8283700c60ade
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-01 10:06:28 +0200

    create project page project card, get the project info, style all the scructure, add icon animations and elements hovers

---
+++ src/components/ExtendedProjectCard/ExtendedProjectCard.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faPaperclip } from '@fortawesome/free-solid-svg-icons';
+import presentation from '/presentation.svg';
+import './ExtendedProjectCard.css'
+export default function ExtendedProjectCard({name, img, description, tags, repoLink, presentationLink, projectLink}) {
+  return (
+    <div className='extendedProjectCard w-full h-[250px] flex'>
+        <img src={img} alt="project image" className='w-[40%]' />
+        <div className="infoContainer w-[58%] ml-[2%]">
+            <div className="projectNameCon w-full flex items-center">
+                <h3 className='text-customWhite text-3xl font-medium'>{name}</h3>
+                <div className="separatingLine w-[2px] h-[25px] rounded-sm bg-customPurple mx-5"></div>
+                <div className="projectLinks flex">
+                <a 
+                    href={repoLink} 
+                    className='mr-5 presentationIcon' 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    <FontAwesomeIcon 
+                        icon={faGithub} 
+                        size="2x" 
+                        className="text-customPurple" 
+                    />
+                </a>
+                <a 
+    href={presentationLink} 
+    className='mr-5' 
+    target="_blank" 
+    rel="noopener noreferrer"
+>
+    <img 
+        src={presentation} 
+        alt="presentation icon" 
+        className="presentationIcon hover:scale-110 transition-transform duration-300" 
+    />
+                </a>
+                <a 
+                    href={projectLink} 
+                    className='mr-5 presentationIcon' 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    <FontAwesomeIcon 
+                      icon={faPaperclip} 
+                      size="2x" 
+                      className="text-customPurple" 
+                    />
+                </a>
+                </div>
+            </div>
+            <p className="projectDesctiption w-[90%] max-h-28 text-customWhite font-light text-sm mt-3">{description}</p>
+            <div className="tagsContainer w-full flex flex-wrap max-h-24">
+                {tags.map((tag, index) => (
+                    <p key={index} className="tag text-customWhite font-light text-sm p-3 bg-customBgPurple flex items-center justify-center rounded-2xl cursor-pointer mt-5 mr-4 hover:bg-customPurple transition-all duration-300">#{tag}</p>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import ExtendedProjectCard from '../ExtendedProjectCard/ExtendedProjectCard'
+import sheret from '/projectsImgs/sheret.png';
+import travelog from '/projectsImgs/travelog.png';
+import dental from '/projectsImgs/dental.png';
+import honey from '/projectsImgs/honey.png';
+import yantarne from '/projectsImgs/yantarne.png';
+import yorokobi from '/projectsImgs/yorokobi.png';
 export default function Projects() {
+  let projects = [
+    {
+      name: 'SHERET',
+      img: sheret,
+      description: 'Sheret is an online file-sharing platform designed to make managing your files effortless and hassle-free—no need for physical storage devices. With a sleek, user-friendly interface, my custom design, and incredibly convenient features, Sheret has everything you need. Simply upload your file, receive a unique code, and access your file anytime, anywhere!',
+      tags: ['HTML', 'CSS', 'JS', 'Node.js', 'Multer', 'Express', 'Dotenv', 'MongoDB'],
+      repoLink: 'https://github.com/ZubalYana/Sheret',
+      presentationLink: 'https://www.canva.com/design/DAGWAE-TrWU/PggvoTljiQ5zdtjk9Rzj7A/edit?utm_content=DAGWAE-TrWU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton',
+      projectLink: 'https://sheret.onrender.com/'
+    },
+  ]
   return (
-    <div className="wrap w-full min-h-screen bg-customBlack">
+    <div className="wrap w-full min-h-screen bg-customBlack p-10 flex flex-col items-center">
+       {projects.map((project, index) => (
+        <ExtendedProjectCard key={index} {...project} /> 
+      ))}
     </div>
   )
 }
