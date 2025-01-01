a7b3c9349714a554377a92f8b99aad49e2871358
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-01 10:46:42 +0200

    fill the basic projects in the array, adjust styling and distances

--- src/components/ExtendedProjectCard/ExtendedProjectCard.jsx
+++ src/components/ExtendedProjectCard/ExtendedProjectCard.jsx
@@ -6,7 +6,7 @@ import presentation from '/presentation.svg';
 import './ExtendedProjectCard.css'
 export default function ExtendedProjectCard({name, img, description, tags, repoLink, presentationLink, projectLink}) {
   return (
-    <div className='extendedProjectCard w-full h-[250px] flex'>
+    <div className='extendedProjectCard w-full h-[250px] flex mb-11'>
         <img src={img} alt="project image" className='w-[40%]' />
         <div className="infoContainer w-[58%] ml-[2%]">
             <div className="projectNameCon w-full flex items-center">
@@ -25,36 +25,40 @@ export default function ExtendedProjectCard({name, img, description, tags, repoL
                         className="text-customPurple" 
                     />
                 </a>
-                <a 
-    href={presentationLink} 
-    className='mr-5' 
-    target="_blank" 
-    rel="noopener noreferrer"
->
-    <img 
-        src={presentation} 
-        alt="presentation icon" 
-        className="presentationIcon hover:scale-110 transition-transform duration-300" 
-    />
-                </a>
-                <a 
-                    href={projectLink} 
-                    className='mr-5 presentationIcon' 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                >
-                    <FontAwesomeIcon 
-                      icon={faPaperclip} 
-                      size="2x" 
-                      className="text-customPurple" 
-                    />
-                </a>
+                {presentationLink && (
+                    <a 
+                        href={presentationLink} 
+                        className='mr-5' 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        <img 
+                            src={presentation} 
+                            alt="presentation icon" 
+                            className="presentationIcon hover:scale-110 transition-transform duration-300" 
+                        />
+                    </a>
+                )}
+                {projectLink && (
+                    <a 
+                        href={projectLink} 
+                        className='mr-5 presentationIcon' 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        <FontAwesomeIcon 
+                            icon={faPaperclip} 
+                            size="2x" 
+                            className="text-customPurple" 
+                        />
+                    </a>
+                )}
                 </div>
             </div>
             <p className="projectDesctiption w-[90%] max-h-28 text-customWhite font-light text-sm mt-3">{description}</p>
-            <div className="tagsContainer w-full flex flex-wrap max-h-24">
+            <div className="tagsContainer w-[90%] flex flex-wrap max-h-24">
                 {tags.map((tag, index) => (
-                    <p key={index} className="tag text-customWhite font-light text-sm p-3 bg-customBgPurple flex items-center justify-center rounded-2xl cursor-pointer mt-5 mr-4 hover:bg-customPurple transition-all duration-300">#{tag}</p>
+                    <p key={index} className="tag text-customWhite font-light text-sm p-3 bg-customBgPurple flex items-center justify-center rounded-2xl cursor-pointer mt-4 mr-4 hover:bg-customPurple transition-all duration-300">#{tag}</p>
                 ))}
             </div>
         </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -17,6 +17,52 @@ export default function Projects() {
       presentationLink: 'https://www.canva.com/design/DAGWAE-TrWU/PggvoTljiQ5zdtjk9Rzj7A/edit?utm_content=DAGWAE-TrWU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton',
       projectLink: 'https://sheret.onrender.com/'
     },
+    {
+      name: 'Travelog',
+      img: travelog,
+      description: 'This webpage is for a dental clinic and offers a range of functions with a user-friendly experience. It was my first major React project, featuring tools for patients to leave reviews, register, and book appointments. On the administrative side, it includes an intuitive admin panel to manage the clinic\'s services efficiently. This project was a valuable step in building dynamic and interactive web applications.',
+      tags: ['HTML', 'CSS', 'JS', 'Node.js', 'Multer', 'Express', 'Dotenv', 'MongoDB', 'Bcrypt', 'JWT', 'Cookies'],
+      repoLink: 'https://github.com/ZubalYana/Personal-blog',
+      presentationLink: 'https://www.canva.com/design/DAGPoAozo18/CaHcLiOBT8_yW07VmK_qrQ/edit',
+      projectLink: ''
+    },
+    {
+      name: 'DENTAL',
+      img: dental,
+      description: 'This webpage is for a dental clinic and offers a range of functions with a user-friendly experience. It was my first major React project, featuring tools for patients to leave reviews, register, and book appointments. On the administrative side, it includes an intuitive admin panel to manage the clinic\'s services efficiently. This project was a valuable step in building dynamic and interactive web applications.',
+      tags: ['HTML', 'CSS', 'JS', 'Node.js', 'Multer', 'Express', 'Dotenv', 'MongoDB', 'React'],
+      repoLink: 'https://github.com/ZubalYana/Dental',
+      presentationLink: '',
+      projectLink: ''
+    },
+    {
+      name: 'Honey',
+      img: honey,
+      description: 'This project is a static web page I created to learn and practice using the Tailwind CSS framework. Through building this page, I gained valuable hands-on experience with Tailwind\'s utility-first approach to styling, which has since become a key part of my workflow. While this page doesn\'t include real functionality and was completed in just one day, it served as a great starting point for mastering the framework.',
+      tags: ['HTML', 'CSS', 'JS', 'Node.js', 'React', 'Tailwind CSS'],
+      repoLink: 'https://github.com/ZubalYana/Honey-Tailwind-practice',
+      presentationLink: '',
+      projectLink: ''
+    },
+    {
+      name: 'Yantarne FM',
+      img: yantarne,
+      description: 'This was a team project where I took charge of designing and developing the first screen, focusing on responsive design and engaging animations—something I truly enjoy in every project. Working with the team helped me strengthen my collaboration skills, and we integrated a radio API to create our unique version. This experience was both rewarding and a great opportunity to combine creativity with teamwork.',
+      tags: ['HTML', 'CSS', 'JS', 'Node.js', 'React', 'API'],
+      repoLink: 'https://github.com/ihymon/yantarne-team-project',
+      presentationLink: '',
+      projectLink: ''
+    },
+    {
+      name: 'Yorokobi',
+      img: yorokobi,
+      description: 'Yorokobi is a Japanese cuisine restaurant concept born from my passion for Japanese culture and vivid imagination. While it may not match the sophistication or functionality of my more recent projects, it holds a special place in my journey as a developer. This project marked a significant milestone, allowing me to combine creativity with technical skills, and it was one I truly enjoyed and felt proud of completing.',
+      tags: ['HTML', 'CSS', 'JS', 'Node.js', 'Express', 'Axios', 'MongoDB'],
+      repoLink: 'https://github.com/ZubalYana/Yorokobi',
+      presentationLink: '',
+      projectLink: ''
+    },
+
   ]
   return (
     <div className="wrap w-full min-h-screen bg-customBlack p-10 flex flex-col items-center">
