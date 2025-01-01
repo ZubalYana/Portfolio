4a3565f1798ab592568b1929d60e81b1a1ff31dc
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-01 15:09:36 +0200

    adjust the basic theme changing logic

--- src/components/SettingsPopup/SettingsPopup.jsx
+++ src/components/SettingsPopup/SettingsPopup.jsx
@@ -1,46 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Modal from 'react-modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import ThemeContext from '../../ThemeContext'; 
 import './SettingsPopup.css';
 import gear1 from '/gear1.svg';
 import gear2 from '/gear2.svg';
 import gear3 from '/gear3.svg';
 import gear4 from '/gear4.svg';
-Modal.setAppElement('#root'); 
+
+Modal.setAppElement('#root');
 
 const SettingsPopup = ({ isOpen, onClose }) => {
+  const { theme, toggleTheme } = useContext(ThemeContext); 
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onClose}
       contentLabel="Settings Popup"
       style={{
-        overlay: { 
-          backgroundColor: 'rgba(0, 0, 0, 0.5)', 
-          display: 'flex', 
-          justifyContent: 'center', 
+        overlay: {
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          display: 'flex',
+          justifyContent: 'center',
           alignItems: 'center',
         },
-        content: { 
-          padding: '25px', 
-          borderRadius: '16px', 
-          width: '600px', 
+        content: {
+          padding: '25px',
+          borderRadius: '16px',
+          width: '600px',
           height: '290px',
           position: 'absolute',
           top: '50%',
           left: '50%',
           transform: 'translate(-50%, -50%)',
-          color: '#1F1F1F',
+          color: theme === 'dark' ? '#FFF' : '#1F1F1F',
+          backgroundColor: theme === 'dark' ? '#1F1F1F' : '#FFF',
           overflow: 'hidden',
         },
       }}
     >
       <FontAwesomeIcon icon={faXmark} className="closeButton absolute top-[25px] right-[25px] cursor-pointer" onClick={onClose} size="2x" />
-      <h2 className='popupTitle text-4xl font-normal mb-4'>{'<Settings/>'}</h2>
+      <h2 className="popupTitle text-4xl font-normal mb-4">{'<Settings/>'}</h2>
       <div className="settingGroup flex mb-3">
-        <p className='font-light text-xl mr-1'>Page language:</p>
-        <select id="language" className='font-medium text-xl'>
+        <p className="font-light text-xl mr-1">Page language:</p>
+        <select id="language" className="font-medium text-xl">
           <option value="en" selected>Eng.</option>
           <option value="ua">Укр.</option>
           <option value="fr">Fr.</option>
@@ -49,8 +53,13 @@ const SettingsPopup = ({ isOpen, onClose }) => {
         </select>
       </div>
       <div className="settingGroup flex">
-        <p className='font-light text-xl mr-1'>Page theme:</p>
-        <p className='font-medium text-xl cursor-pointer'>dark ( default )</p>
+        <p className="font-light text-xl mr-1">Page theme:</p>
+        <p
+          className="font-medium text-xl cursor-pointer"
+          onClick={toggleTheme}
+        >
+          {theme === 'dark' ? 'Switch to light' : 'Switch to dark'}
+        </p>
       </div>
       <img src={gear1} alt="gear1" className="absolute bottom-[-20%] right-[-10%] rotatingGear" />
       <img src={gear2} alt="gear2" className="absolute bottom-[-10%] left-[61%] rotatingGear" />
