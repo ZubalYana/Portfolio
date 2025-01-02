e8826f63459bf97c2b4f2a68817e517cdad2b020
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-02 15:13:37 +0200

    translate the third screen

--- src/components/EducationCards.jsx
+++ src/components/EducationCards.jsx
@@ -4,15 +4,33 @@ import creatorLogo from '/creatorLogo.svg';
 import 'animate.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { useTranslation } from 'react-i18next';
+
 export default function EducationCards() {
+  let { t, i18n } = useTranslation();
+  let descriptions = [
+    {
+      description: "I graduated from the secondary <span className='font-medium'>school of Shklo</span> in Ukraine with red diploma and multiply won competitions",
+      uaDescription: "Я закінчила <span className='font-medium'>Шклівську школу</span> у Україні з червоним дипломом і багатьма нагородами",
+      frDescription: "Je suis diplômée de l'<span className='font-medium'>Ecole secondaire de Shklo</span> en Ukraine avec un diplome rouge et plusieurs prix",
+    },
+    {
+      description: "I finished modules: HTML/CSS, JavaScript, Node.js and React in <span className='font-medium'>Creator IT Academy</span> and still continue my education there",
+      uaDescription: "Я закінчила модулі: HTML/CSS, JavaScript, Node.js та React у <span className='font-medium'>Creator IT Academy</span> та продовжую навчатися там",
+      frDescription: "J'ai terminé les modules : HTML/CSS, JavaScript, Node.js et React à <span className='font-medium'>Creator IT Academy</span> et je continue toujours mes études là-bas",
+    },
+  ];
+
   const [isAnimatingHat, setIsAnimatingHat] = useState(false);
   const [isAnimatingLogo, setIsAnimatingLogo] = useState(false);
+
   useEffect(() => {
     AOS.init({
       duration: 1000, 
       offset: 200,
     });
   }, []);
+
   const handleHatClick = () => {
     setIsAnimatingHat(true);
     setTimeout(() => {
@@ -27,30 +45,35 @@ export default function EducationCards() {
     }, 1000); 
   };
 
+  // Function to get the appropriate description based on the current language
+  const getDescription = (descriptions) => {
+    switch (i18n.language) {
+      case 'ua':
+        return descriptions.uaDescription;
+      case 'fr':
+        return descriptions.frDescription;
+      default:
+        return descriptions.description;
+    }
+  };
+
   return (
     <div className='eduCardsContainer h-[93%] w-[50%] mt-6 flex flex-col items-center justify-around'>
-      <div className="eduCard w-[380px] h-[240px] rounded-2xl border-customPurple border-4 p-5 flex flex-col items-center justify-center" data-aos="fade-down-left">
-        <img
-          src={graduationHat}
-          alt="graduation logo"
-          className={` ${isAnimatingHat ? 'animate__animated animate__swing' : ''}`}
-          onClick={handleHatClick}
-        />
-        <p className='text-customWhite font-light text-sm text-center mt-5'>
-          I graduated from the secondary <span className='font-medium'>school of Shklo</span> in Ukraine with red diploma and multiply won competitions
-        </p>
-      </div>
-      <div className="eduCard w-[380px] h-[240px] rounded-2xl border-customPurple border-4 p-5 flex flex-col items-center justify-center" data-aos="fade-down-left">
-        <img
-          src={creatorLogo}
-          alt="creator logo"
-          className={` ${isAnimatingLogo ? 'animate__animated animate__swing' : ''}`}
-          onClick={handleLogoClick}
-        />
-        <p className='text-customWhite font-light text-sm text-center mt-5'>
-          I finished modules: HTML/CSS, JavaScript, Node.js and React in <span className='font-medium'>Creator IT Academy</span> and still continue my education there
-        </p>
-      </div>
+      {descriptions.map((desc, index) => (
+        <div 
+          key={index} 
+          className="eduCard w-[380px] h-[240px] rounded-2xl border-customPurple border-4 p-5 flex flex-col items-center justify-center" 
+          data-aos="fade-down-left"
+        >
+          <img
+            src={index === 0 ? graduationHat : creatorLogo}
+            alt={index === 0 ? "graduation logo" : "creator logo"}
+            className={` ${index === 0 ? isAnimatingHat : isAnimatingLogo ? 'animate__animated animate__swing' : ''}`}
+            onClick={index === 0 ? handleHatClick : handleLogoClick}
+          />
+          <p className='text-customWhite font-light text-sm text-center mt-5' dangerouslySetInnerHTML={{ __html: getDescription(desc) }} />
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/ExperienceRoadmap/ExperienceRoadmap.jsx
+++ src/components/ExperienceRoadmap/ExperienceRoadmap.jsx
@@ -1,33 +1,60 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import './ExperienceRoadmap.css';
 
 export default function ExperienceRoadmap() {
+  const { t, i18n } = useTranslation();
   const steps = [
     {
       year: '2022',
       title: 'I began my journey',
       description:
         'by learning the basics of HTML and CSS at Creator IT Academy. It was there that I first got a taste of programming and quickly developed a liking for it. At the time, I thought it would just be a hobby. This year I experienced JavaScript as well. My first programming language!',
+      uaTitle: 'Я розпочала свій шлях',
+      uaDescription:
+        'вивчаючи основи HTML та CSS в Академії Creator IT. Саме там я вперше доторкнулася до програмування і швидко захопилася цим. Тоді я думала, що це буде лише хобі. Цього року я також познайомилася з JavaScript. Моя перша мова програмування!',
+      frTitle: 'J’ai commencé mon parcours',
+      frDescription:
+        "en apprenant les bases de HTML et CSS à Creator IT Academy. C’est là que j’ai eu un premier aperçu de la programmation et que j’ai rapidement développé une passion pour ça. À l’époque, je pensais que ce serait juste un passe-temps. Cette année-là, j’ai aussi découvert JavaScript, mon premier langage de programmation !",
     },
     {
       year: '2023',
       title: 'I continued my path',
       description:
         'with constant learning, diving into the basics of JavaScript and building simple web pages. I also started using GitHub and Loom, and got acquainted with web design. After taking a few web design courses at Creator IT Academy, my design way was set in motion!',
+      uaTitle: 'Я продовжила свій шлях',
+      uaDescription:
+        'з постійним навчанням, занурюючись у основи JavaScript і створюючи прості веб-сторінки. Я також почала використовувати GitHub і Loom, а також ознайомилася з веб-дизайном. Після кількох курсів з веб-дизайну в Академії Creator IT мій шлях у дизайні був визначений!',
+      frTitle: 'J’ai poursuivi mon chemin',
+      frDescription:
+        "avec un apprentissage constant, plongeant dans les bases de JavaScript et créant des pages web simples. J’ai aussi commencé à utiliser GitHub et Loom, et j’ai découvert le design web. Après avoir suivi quelques cours de design web à Creator IT Academy, ma voie dans le design a été lancée !",
     },
     {
       year: '2024',
       title: 'My experience',
       description:
         'grew steadily throughout the year as I completed various projects and faced many challenges along the way. I made valuable connections in the web development sphere, began working with Node.js, and learned a wide range of tools and technologies. Furthermore, I started learning React!',
+      uaTitle: 'Мій досвід',
+      uaDescription:
+        'зростав поступово протягом року, коли я завершувала різноманітні проекти та стикалася з багатьма викликами. Я завела корисні контакти у сфері веб-розробки, почала працювати з Node.js і освоїла безліч інструментів і технологій. Крім того, я почала вивчати React!',
+      frTitle: 'Mon expérience',
+      frDescription:
+        "a progressé régulièrement tout au long de l’année alors que j’ai réalisé divers projets et affronté de nombreux défis. J’ai fait de précieux contacts dans le domaine du développement web, commencé à travailler avec Node.js et appris une large gamme d’outils et de technologies. De plus, j’ai commencé à apprendre React !",
     },
     {
       year: '2025',
       title: 'See you then!',
       description:
         'The coming year will undoubtedly bring me plenty of new experiences and knowledge as I continue to dive deeper into programming. I aim to enhance my coding and design skills, complete many new projects, and possibly explore animation and Photoshop. Who knows what opportunities await?',
+      uaTitle: 'До зустрічі!',
+      uaDescription:
+        'Наступний рік, безсумнівно, принесе мені безліч нових вражень і знань, оскільки я продовжую поглиблювати свої знання в програмуванні. Моя мета — покращити свої навички кодування та дизайну, завершити багато нових проектів і, можливо, дослідити анімацію та Photoshop. Хто знає, які можливості чекають?',
+      frTitle: 'À bientôt!',
+      frDescription:
+        "L’année à venir m’apportera sans aucun doute de nombreuses nouvelles expériences et connaissances alors que je continue à plonger plus profondément dans la programmation. Je vise à améliorer mes compétences en codage et en design, réaliser de nombreux nouveaux projets et peut-être explorer l’animation et Photoshop. Qui sait quelles opportunités m’attendent ?",
     },
   ];
+  
 
   const [activeStep, setActiveStep] = useState(-1);
   const containerRefs = useRef([]);
@@ -48,12 +75,12 @@ export default function ExperienceRoadmap() {
     );
 
     containerRefs.current
-      .filter((ref) => ref) 
+      .filter((ref) => ref)
       .forEach((ref) => observer.observe(ref));
 
     return () => {
       containerRefs.current
-        .filter((ref) => ref) 
+        .filter((ref) => ref)
         .forEach((ref) => observer.unobserve(ref));
     };
   }, [activeStep]);
@@ -84,10 +111,10 @@ export default function ExperienceRoadmap() {
           </div>
           <div className="experience_text w-[650px] h-full flex flex-col pl-5">
             <div className="experience_title text-2xl font-medium text-customWhite">
-              {step.title}
+              {t(step[`${i18n.language}Title`] || step.title)}
             </div>
             <div className="experience_description text-sm font-light text-customWhite mt-2">
-              {step.description}
+              {t(step[`${i18n.language}Description`] || step.description)}
             </div>
           </div>
         </div>
