6a9c57b4a6ef4edf203fc212f1f5f219feca6896
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-01-08 20:06:43 +0200

    adapt the faq screen for avarage phones

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
+import './Questions.css';
 export default function Questions() {
   const { i18n } = useTranslation();
   const [activeIndex, setActiveIndex] = useState(null);
