dd5884dc1012bcc0fcd3faaae9025151deb006f3
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-03-31 05:16:18 +0300

    insure proper resposnive design concerning the development tag

--- src/components/ExtendedProjectCard/ExtendedProjectCard.jsx
+++ src/components/ExtendedProjectCard/ExtendedProjectCard.jsx
@@ -14,63 +14,65 @@ export default function ExtendedProjectCard({ name, img, description, tags, repo
                 <div className="projectNameCon w-full flex items-center">
                     <h3 className='text-customWhite text-3xl font-medium'>{name}</h3>
                     <div className="separatingLine w-[2px] h-[25px] rounded-sm bg-customPurple mx-5"></div>
-                    <div className="projectLinks flex">
-                        <a
-                            href={repoLink}
-                            className='mr-5 presentationIcon'
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <FontAwesomeIcon
-                                icon={faGithub}
-                                size="2x"
-                                className="text-customPurple"
-                            />
-                        </a>
-                        {presentationLink && (
+                    <div className="flex flex-col lg:flex-row">
+                        <div className='projectLinks flex'>
                             <a
-                                href={presentationLink}
-                                className='mr-5'
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <img
-                                    src={presentation}
-                                    alt="presentation icon"
-                                    className="presentationIcon hover:scale-110 transition-transform duration-300"
-                                />
-                            </a>
-                        )}
-                        {projectLink && (
-                            <a
-                                href={projectLink}
+                                href={repoLink}
                                 className='mr-5 presentationIcon'
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >
                                 <FontAwesomeIcon
-                                    icon={faLink}
+                                    icon={faGithub}
                                     size="2x"
                                     className="text-customPurple"
                                 />
                             </a>
-                        )}
-                        {videoPresentation && (
-                            <a
-                                href={videoPresentation}
-                                className='mr-5 presentationIcon'
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <FontAwesomeIcon
-                                    icon={faCirclePlay}
-                                    size="2x"
-                                    className="text-customPurple"
-                                />
-                            </a>
-                        )}
+                            {presentationLink && (
+                                <a
+                                    href={presentationLink}
+                                    className='mr-5'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <img
+                                        src={presentation}
+                                        alt="presentation icon"
+                                        className="presentationIcon hover:scale-110 transition-transform duration-300"
+                                    />
+                                </a>
+                            )}
+                            {projectLink && (
+                                <a
+                                    href={projectLink}
+                                    className='mr-5 presentationIcon'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <FontAwesomeIcon
+                                        icon={faLink}
+                                        size="2x"
+                                        className="text-customPurple"
+                                    />
+                                </a>
+                            )}
+                            {videoPresentation && (
+                                <a
+                                    href={videoPresentation}
+                                    className='mr-5 presentationIcon'
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <FontAwesomeIcon
+                                        icon={faCirclePlay}
+                                        size="2x"
+                                        className="text-customPurple"
+                                    />
+                                </a>
+                            )}
+                        </div>
                         {inDevelopment && (
-                            <div className='ml-5 min-w-[200px] w-auto flex text-customWhite items-center justify-center rounded-2xl'>
+                            <div className='ml-0 mt-3 min-w-[200px] w-auto flex text-customWhite items-center justify-center rounded-2xl lg:ml-5 lg:mt-0'>
                                 <div className='w-[10px] h-[10px] rounded-full bg-red-700 mr-2 animate-pulse'></div>
                                 <Trans i18nKey="currentlyInDevelopment" />
                             </div>
